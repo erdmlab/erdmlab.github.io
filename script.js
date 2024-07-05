37a37affd34f3d94af19a04046bd261e4c8be6ab
ErdemAkz <erdemsergeant@gmail.com>
2024-07-05 19:59:39 +0300

    asd

---
+++ script.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+    if (isMobile) {
+        $('.book').turn({
+            width: $(window).width(),
+            height: $(window).height() - 60,
+            autoCenter: true
+        });
+
+        $('#next').hide();
+        $('#prev').hide();
+
+        $(document).on('swipeleft', function() {
+            $('.book').turn('next');
+        });
+
+        $(document).on('swiperight', function() {
+            $('.book').turn('previous');
+        });
+
+        // Ensure the orientation is horizontal
+        window.addEventListener("orientationchange", function() {
+            if (window.orientation !== 90 && window.orientation !== -90) {
+                alert("Please rotate your device to landscape mode for the best experience.");
+            }
+        });
+
+        // Check initial orientation
+        if (window.orientation !== 90 && window.orientation !== -90) {
+            alert("Please rotate your device to landscape mode for the best experience.");
+        }
+    } else {
+        $('.book').turn({
+            width: 600,
+            height: 400,
+            autoCenter: true
+        });
+
+        $('#next').click(function() {
+            $('.book').turn('next');
+        });
+
+        $('#prev').click(function() {
+            $('.book').turn('previous');
+        });
+    }
+});
